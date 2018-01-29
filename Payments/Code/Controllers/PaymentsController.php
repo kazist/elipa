b207ca4024cf6eb6f16acd2e831954a8719395f4
dedanirungu <dedanirungu@gmail.com>
2018-01-29 06:44:08 +0300

    Improving the way Resellerclub works

---
+++ Payments/Code/Controllers/PaymentsController.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Kazist Framework.
+ * (c) Dedan Irungu <irungudedan@gmail.com>
+ *  For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ * 
+ */
+
+/**
+ * Description of PaymentsController
+ *
+ * @author sbc
+ */
+
+namespace Elipa\Payments\Code\Controllers;
+
+defined('KAZIST') or exit('Not Kazist Framework');
+
+use Kazist\Controller\BaseController;
+use Elipa\Payments\Code\Models\ElipaModel;
+use Payments\Payments\Code\Controllers\PaymentsController AS BasePaymentsController;
+
+class PaymentsController extends BasePaymentsController {
+
+    public function cancelAction() {
+
+        $payment_id = $this->request->query->get('id');
+
+        $this->model = new ElipaModel();
+        $this->model->cancelTransaction($payment_id);
+        $payment_url = $this->model->getUrlByPaymentId($payment_id);
+
+        return $this->redirect($payment_url);
+    }
+
+    public function returnAction() {
+
+        $payment_id = $this->request->query->get('id');
+
+        $this->model = new ElipaModel();
+        $this->model->completeTransaction($payment_id);
+        $payment_url = $this->model->getUrlByPaymentId($payment_id);
+
+        return $this->redirect($payment_url);
+    }
+
+    public function notifyAction() {
+
+        $payment_id = $this->request->query->get('id');
+
+        $this->model = new ElipaModel();
+        $this->model->processElipa($payment_id);
+        $this->model->notificationTransaction($payment_id);
+        $payment_url = $this->model->getUrlByPaymentId($payment_id);
+
+        return $this->redirect($payment_url);
+    }
+
+}
